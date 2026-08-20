4298ade275ee4cd2d76695a9d34ed290a393903d
freddie <rincofrederico@gmail.com>
2026-08-20 19:02:40 +0100

    Modernize React Router template runtime
    Assisted-By: devx/46c44251-bc51-4b95-afdc-6afd5afa0f89

--- templates/react-router/env.d.ts
+++ templates/react-router/env.d.ts
@@ -4,16 +4,6 @@
 
 import type { Env as AppEnv } from "./app/lib/env";
 
-type ReactRouterExampleContext = {
-  env: Env;
-  waitUntil: ExecutionContext["waitUntil"];
-};
-
-declare module "react-router" {
-  interface AppLoadContext extends ReactRouterExampleContext {}
-  interface RouterContextProvider extends ReactRouterExampleContext {}
-}
-
 declare global {
   interface Env extends AppEnv {}
 }
